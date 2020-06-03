afdc6d42767163ddb1b758a5795e4d3a6f53c15b
fionagood <65222329+fionagood@users.noreply.github.com>
2020-06-03 18:33:49 +0200

    update

--- 06_blitzlicht/blitzlicht_gif.js
+++ 06_blitzlicht/blitzlicht_gif.js
@@ -1,10 +1,14 @@
+// Der Hintergrund soll schwarz sein und das Bild blinkend
+
 let capture;
 var i = 0
+let ele;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     capture = createCapture(VIDEO);
     //capture.hide();
+    ele = createAudio('assets/binaural_beats_theta_wave.mp3');
 }
 
 function draw() {
@@ -12,12 +16,15 @@ function draw() {
     image(capture, 0, 0, windowWidth, windowHeight);
     filter(GRAY);
 
+    ele.autoplay(true);
+
+
     i = i + 1
 
     // jedes 10. Mal ist die Bedingung erfüllt, blinkt
-    if (i % 15 === 0) {
+    if (i % 5 === 0) {
         background(255);
-    } //else {
+    } else {
         // alle anderen Zeiten, mit 0 auffüllen
         // background(0);
     }
