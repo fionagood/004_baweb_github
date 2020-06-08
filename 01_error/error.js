46d3d1fd9aaaf750ec25ac7fb927043051715c09
fionagood <65222329+fionagood@users.noreply.github.com>
2020-06-08 04:27:44 +0200

    update error, warnung

--- 01_error/error.js
+++ 01_error/error.js
@@ -6,7 +6,7 @@ let img;
 let myFont;
 
 function preload() {
-  myFont = loadFont('../orbitron/orbitron-medium.otf');
+    myFont = loadFont('../orbitron/orbitron-medium.otf');
 }
 
 let reverb
@@ -24,81 +24,81 @@ let otherSide = false;
 let posY;
 
 function setup() {
-  img = loadImage('assets/error.svg');
+    img = loadImage('assets/error.svg');
 
-  // animation an position 0 setzen unter den HTML text
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0, 0);
-  canvas.style('z-index', '-1');
+    // animation an position 0 setzen unter den HTML text
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+    canvas.style('z-index', '-1');
 
-  //   background(0);
+    //   background(0);
 
-  // let cnv = createCanvas(100, 100);
-  // cnv.mousePressed(playSynth);
-  // cnv.background(0)
+    // let cnv = createCanvas(100, 100);
+    // cnv.mousePressed(playSynth);
+    // cnv.background(0)
 
-  // sound
-  env = new p5.Envelope(t1, l1, t2, l2);
-  triOsc = new p5.Oscillator('sine');
-  env.setRange(0.3, 0)
-  reverb = new p5.Reverb();
-  reverb.drywet(1);
-  reverb.process(triOsc, 3, 2);
+    // sound
+    env = new p5.Envelope(t1, l1, t2, l2);
+    triOsc = new p5.Oscillator('sine');
+    env.setRange(0.3, 0)
+    reverb = new p5.Reverb();
+    reverb.drywet(1);
+    reverb.process(triOsc, 3, 2);
 }
 
 // Linie
 function draw() {
-  textFont(myFont);
-  image(img, 0, windowHeight / 2, windowWidth, 100);
-  // ellipse(mouseX, mouseY, 10, 10);
-  noStroke();
-  fill(255);
+    textFont(myFont);
+    image(img, 0, windowHeight / 2, windowWidth, 100);
+    // ellipse(mouseX, mouseY, 10, 10);
+    noStroke();
+    fill(255);
 
 
-  background(0, 100)
-  if (frameCount % 20 == 0) {
-    background('red')
-    playSynth()
-    textSize(50)
-  }
+    background(0, 100)
+    if (frameCount % 20 == 0) {
+        background('red')
+        playSynth()
+        textSize(50)
+    }
 
-  // Linie
-  text(int(frameCount / 10 + mouseX / 10) % 20, width / 2, 100);
-  rect(mouseY * 2, 0, 10, height);
+    // Linie
+    text(int(frameCount / 10 + mouseX / 10) % 20, width / 2, 100);
+    rect(mouseY * 2, 0, 10, height);
 
-  // wenn du d bewegigsrichtig vo de line im verhältnis zu de muus
-  // ändere möchtsch, denn chasch efach die beide letshte argument
-  // bi posY vertuusche, aso dass es nachher so usgseht:
-  // posY = map(mouseY, 0, height, 0, width);
-  posY = map(mouseY, 0, height, width, 0);
-  line(posY, 0, posY, height);
+    // wenn du d bewegigsrichtig vo de line im verhältnis zu de muus
+    // ändere möchtsch, denn chasch efach die beide letshte argument
+    // bi posY vertuusche, aso dass es nachher so usgseht:
+    // posY = map(mouseY, 0, height, 0, width);
+    posY = map(mouseY, 0, height, width, 0);
+    line(posY, 0, posY, height);
 
-  if (posY < 20 && !otherSide) {
-    countL++;
-    otherSide = true;
-  } else if (posY > width - 20 && otherSide) {
-    countR++;
-    otherSide = false;
-  }
+    if (posY < 20 && !otherSide) {
+        countL++;
+        otherSide = true;
+    } else if (posY > width - 20 && otherSide) {
+        countR++;
+        otherSide = false;
+    }
 
-  if (countR + countL > 5) {
-    alert("OK bye");
-    window.location.href = ' ../02_timer/timer.html';
-  }
+    if (countR + countL > 2) {
+        alert("Sie haben den Fehler behoben.");
+        window.location.href = '../04_random_words/random_words.html';
+    }
 }
 
 
 function playSynth() {
-  triOsc.freq(100 * pow(pow(2, 1 / 12), int(frameCount / 10 + mouseX / 10) % 20));
-  triOsc.start();
-  // triOsc.amp(0.2);
-  env.play(triOsc);
+    triOsc.freq(100 * pow(pow(2, 1 / 12), int(frameCount / 10 + mouseX / 10) % 20));
+    triOsc.start();
+    // triOsc.amp(0.2);
+    env.play(triOsc);
 }
 
 function mouseMoved() {
-  playSynth()
+    playSynth()
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+    resizeCanvas(windowWidth, windowHeight);
 }
--- 01_error/error_home.js
+++ 01_error/error_home.js
@@ -19,6 +19,4 @@ function draw() {
     noStroke();
     fill(255, 255, 255, 70);
     rect(mouseX, mouseY, 1, height);
-
-
 }
